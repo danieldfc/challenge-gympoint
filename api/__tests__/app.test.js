c3950ec6483d50b27625aef53a3e42b603875d45
Daniel Felizardo <daniel.david772@gmail.com>
2019-10-20 01:09:32 -0300

    Initial commit

---
+++ api/__tests__/app.test.js
@@ -0,0 +1,3 @@
+test('ola', () => {
+  expect(1 + 1).toBe(2);
+});
---
+++ api/src/database/index.js
@@ -0,0 +1,20 @@
+import Sequelize from 'sequelize';
+
+import dbConfig from '../config/database';
+
+const models = [];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(dbConfig);
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+}
+
+export default new Database();
