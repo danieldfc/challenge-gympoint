56aaa4736c7608e3e680d6376ad76176576c9323
Daniel Felizardo <daniel.david772@gmail.com>
2019-10-20 14:01:05 -0300

    feat: Create session to user

--- api/__tests__/integrations/session.test.js
+++ api/__tests__/integrations/session.test.js
@@ -21,4 +21,36 @@ describe('Session Store', () => {
 
     expect(response.body).toHaveProperty('token');
   });
+
+  it('should not be able create a new session without user', async () => {
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: 'test@test.com',
+        password: 'test123',
+      });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should not be able create a new session with password invalid', async () => {
+    const user = await factory.create('User', {
+      password: '123456',
+    });
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '1234567',
+      });
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able create a new session to user', async () => {
+    const response = await request(app).post('/sessions');
+
+    expect(response.status).toBe(403);
+  });
 });
